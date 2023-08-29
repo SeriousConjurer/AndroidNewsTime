1d58c9913976f78c59773b76079bb8488e4c4d16
Himanshu Yadav <86050941+SeriousConjurer@users.noreply.github.com>
2023-08-29 16:07:57 +0530

    removed api key from constant.kt

--- app/src/main/java/com/example/newsapp2/utils/constant.kt
+++ app/src/main/java/com/example/newsapp2/utils/constant.kt
@@ -3,7 +3,7 @@ package com.example.newsapp2.utils
 class constant {
     companion object {
         const val BASE_URL = "https://newsapi.org/"
-        const val API_KEY = "42bce59b35c94596aa4b3a216c100459"
+        const val API_KEY = ""
         val QUERY_ITEM_SIZE = 20
 
 //        42bce59b35c94596aa4b3a216c100459
